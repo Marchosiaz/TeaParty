c4b9c0cf1b98b6090777ebc6aabbdbf6443a8649
Maxim <mark.hruzin@gmail.com>
2020-02-16 16:28:49 +0300

    Updates

---
+++ precache-manifest.d8d267803151fbe278a486f1df26c3a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525bdafc43812a5f5a765f8a662a9529",
+    "url": "/TeaParty/index.html"
+  },
+  {
+    "revision": "86d49145333e216d230f",
+    "url": "/TeaParty/static/css/main.f4958c22.chunk.css"
+  },
+  {
+    "revision": "0022047be924445dcb36",
+    "url": "/TeaParty/static/js/2.51d471b6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/TeaParty/static/js/2.51d471b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d49145333e216d230f",
+    "url": "/TeaParty/static/js/main.7a3f9017.chunk.js"
+  },
+  {
+    "revision": "43a8cb0aa35e1c17b670",
+    "url": "/TeaParty/static/js/runtime-main.011c4895.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TeaParty/static/media/logo.5d5d9eef.svg"
+  }
+]);
